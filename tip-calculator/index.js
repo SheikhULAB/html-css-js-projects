d5cba4c16b1ccc156acdc8de4797b9c1e672aadd
SheikhULAB <tosibalaptop86@gmail.com>
2025-04-07 23:48:59 +0600

    tip calculator

---
+++ tip-calculator/index.js
@@ -0,0 +1,16 @@
+const btnEl = document.getElementById("calculate");
+
+const billInput = document.getElementById("bill");
+
+const tipInput = document.getElementById("tip");
+
+const totalSpan = document.getElementById("total");
+
+btnEl.addEventListener("click", () => {
+   const billValue = billInput.value;
+   const tipValue = tipInput.value;
+
+   const totalValue = billValue * (1+tipValue / 100);
+
+   totalSpan.innerText = totalValue.toFixed(2);
+})
